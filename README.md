6f4d15665927a6182ce6f5e14ff5cc889ba2e16f
jcummingsutk <jcummingsutk@gmail.com>
2022-01-17 13:33:24 -0500

    model built and tweepy configured

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
-# Project Skeleton
+# Analyzing Tweets
 
-1. EDA, Understanding the Data
-
-# Summary
-
-# Shortened Notebook
-
-## Summary of Gradient Boosting Model
+## Summary
+In this project I create a recurrent neural network (RNN) for analysis of tweets. The RNN is trained using
+a couple of LSTM layers and then tweepy is used to collect tweets on a topic. Any topic can be chosen,
+but in this project I currently use Atlanta as a topic.
--- run.py
+++ run.py
@@ -6,28 +6,14 @@ import tensorflow as tf
 from tensorflow.keras import layers
 import re, string
 import pickle
+import tweepy
 
 #import nltk
 #nltk.download("stopwords")
 MAXLEN = 35
 VOCAB_SIZE = 10000
 
-def save_tokenizer(tokenizer):
-    """save the tokenizer for future use"""
-    import pickle
-    with open('src/models/tokenizer.pickle', 'wb') as handle:
-        pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-def load_tokenizer():
-    with open('src/models/tokenizer.pickle', 'rb') as handle:
-        tokenizer = pickle.load(handle)
-    return tokenizer
-
-def shorten_dataset(data, n=5000):
-    """ Shuffle and shorten the dataset in case you want to test """
-    data = data.sample(frac=1)
-    data = data.head(n)
-    return data
 
 def change_sentiments(data):
     """ The sentiment from the input data is 0 for unhappy, 4 for happy. Let's change the 4 to 1 """
@@ -51,11 +37,10 @@ def make_numpy(in_data):
     sentiments = in_data['sentiment'].to_numpy()
     return tweets, sentiments
 
-def seq_padder(tokenizer, tweets):
-    """tokenizes and pads the tweets"""
+def preprocess(tokenizer, tweets):
+    """tokenizes and pads the tweets. note that tweets is a list of strings"""
     from tensorflow.keras.preprocessing.sequence import pad_sequences
     sequences = tokenizer.texts_to_sequences(tweets)
-    print(sequences[0])
     padded = pad_sequences(sequences, truncating='post', padding='post', maxlen=MAXLEN)
     return padded
 
@@ -65,6 +50,21 @@ def make_tokenizer(vocab_size):
     tokenizer = Tokenizer(num_words=VOCAB_SIZE, oov_token='<UNK>')
     tokenizer.fit_on_texts(tweets)
     return tokenizer
+
+def save_tokenizer(tokenizer):
+    """save the tokenizer for future use"""
+    import pickle
+    with open('src/models/tokenizer.pickle', 'wb') as handle:
+        pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+def load_tokenizer():
+    with open('src/models/tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+    return tokenizer
+
+def load_model():
+    saved_model = tf.keras.models.load_model('src/models/tweet_classifier.h5')
+    return saved_model
 
 def create_model():
     """ Create a bidirectional LSTM model for sentiment analysis """
@@ -79,6 +79,12 @@ def create_model():
     ])
     return model
 
+def shorten_dataset(data, n=5000):
+    """ Shuffle and shorten the dataset in case you want to test """
+    data = data.sample(frac=1)
+    data = data.head(n)
+    return data
+
 def batch_data(X_train, y_train, X_test, y_test):
     """Shuffle and batch the training and test data"""
     BATCH_SIZE = 512
@@ -89,11 +95,28 @@ def batch_data(X_train, y_train, X_test, y_test):
     dataset_test = dataset_test.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
     return dataset_train, dataset_test
 
-if __name__ == "__main__":
-    data = pd.read_csv('data/tweets.csv', encoding = 'Latin-1', names=('sentiment','id','date','flag','username','tweet'))
 
-    #data = shorten_dataset(data, 50000)
-    #pred = make_prediction([[6, 148, 72, 125, 40, 0.627, 50]])
-    #print("The test prediction is {}".format(pred))
+def make_predictions(in_tweets):
+    """Makes predictions using the saved model on whether each tweet in a lits of tweets (in_tweets) is positive or negative
+    values in the return list close to 1 are positive, close to 0 are negative """
+    saved_tokenizer = load_tokenizer()
+    saved_model = load_model()
+    processed_tweets = preprocess(saved_tokenizer, in_tweets)
+
+    #processed_dataset_tweets = tf.data.Dataset.from_tensor_slices((np.array(in_tweets)))
+    predictions = saved_model.predict(processed_tweets)
+    return predictions
+
+if __name__ == "__main__":
+    # a little test
+    #in_tweets = ['this movie is terrible', 'I am so happy for my best friend right now!']
+    #saved_tokenizer = load_tokenizer()
+    #saved_model = load_model()
+    #processed_tweets = preprocess(saved_tokenizer, in_tweets)
+    #processed_tweets
+    #print(make_predictions(in_tweets))
+
+    auth = tweepy.OAuthHandler('0Tx6s0fzxsyOrJoqJYM9qxiLM', 'QDVLq49GYkfG8GT3B9DkyVq3MMoMNZk2cAq5cvHZUaptjZj35W')
+
 
 #print(model.predict_proba(transformed_example))
